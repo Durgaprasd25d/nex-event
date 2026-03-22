b39828195dfbd6d472f7e43e76dd01fa767c2b3a
Durgaprasd25d <durgaprasaddalai10@gmail.com>
2026-03-22 16:49:01 +0530

    changed the config file

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const env = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
+const env = (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod') ? 'prod' : 'dev';
 
 const config = {
   dev: {
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const env = import.meta.env.VITE_APP_ENV === 'production' ? 'prod' : 'dev';
+const env = (import.meta.env.VITE_APP_ENV === 'production' || import.meta.env.VITE_APP_ENV === 'prod') ? 'prod' : 'dev';
 
 const config = {
   dev: {
